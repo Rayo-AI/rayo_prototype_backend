57e5ecb651957e3446296c117945552e06904d3b
Hephzibah Owoeye <ifelizzy5@gmail.com>
2026-05-24 14:39:08 +0100

    fix: aligning frontend and backend

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { build as esbuild } from "esbuild";
 import esbuildPluginPino from "esbuild-plugin-pino";
-import { rm } from "node:fs/promises";
+import { cp, mkdir, rm } from "node:fs/promises";
 
 // Plugins (e.g. 'esbuild-plugin-pino') may use `require` to resolve dependencies
 globalThis.require = createRequire(import.meta.url);
@@ -13,6 +13,7 @@ const artifactDir = path.dirname(fileURLToPath(import.meta.url));
 async function buildAll() {
   const distDir = path.resolve(artifactDir, "dist");
   await rm(distDir, { recursive: true, force: true });
+  await mkdir(distDir, { recursive: true });
 
   await esbuild({
     entryPoints: [
@@ -123,6 +124,10 @@ globalThis.__dirname = __bannerPath.dirname(globalThis.__filename);
     `,
     },
   });
+
+  const templateSourceDir = path.resolve(artifactDir, "src/services/templates");
+  const templateTargetDir = path.resolve(distDir, "templates");
+  await cp(templateSourceDir, templateTargetDir, { recursive: true });
 }
 
 buildAll().catch((err) => {
